deb58a41b194f7601f21c224d483471c7fd9ed92
Covadevs <covadevs@gmail.com>
2022-04-02 23:35:13 -0300

    Correção no metodo que determina a distancia entre dois pontos

--- src/main/java/br/com/zarus/zarus_engine/utils/MathUtils.java
+++ src/main/java/br/com/zarus/zarus_engine/utils/MathUtils.java
@@ -8,7 +8,7 @@ public class MathUtils {
     }
 
     public static double distance(Vector2D p1, Vector2D p2) {
-        return Math.hypot(Math.abs(p2.getX() - p1.getX()), p2.getY() - p1.getY());
+        return Math.hypot(Math.abs(p2.getX() - p1.getX()), Math.abs(p2.getY() - p1.getY()));
     }
 
 }
